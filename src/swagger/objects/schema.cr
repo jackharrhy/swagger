88c07ed101138d4b98636426f7552593cb93b0cd
Jack Arthur Harrhy <me@jackharrhy.com>
2020-06-11 19:50:13 -0230

    add items argument to schema, so an array of refs can be generated

--- src/swagger/objects/schema.cr
+++ src/swagger/objects/schema.cr
@@ -10,7 +10,15 @@ module Swagger::Objects
     include JSON::Serializable
 
     def self.use_reference(name : String)
-      new(ref: "#/components/schemas/#{name}")
+      new(ref: format_reference(name))
+    end
+
+    def self.format_reference(name : String)
+      "#/components/schemas/#{name}"
+    end
+
+    def self.array_of_object(ref : String)
+      new(type: "array", items: {"$ref": format_reference(ref)})
     end
 
     # See https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.3.md#dataTypes
@@ -19,13 +27,15 @@ module Swagger::Objects
     getter required : Array(String)? = nil
     getter default : (String | Int32 | Int64 | Float64 | Bool)? = nil
     getter properties : Hash(String, Property)? = nil
+    getter items : NamedTuple("$ref": String)?
 
     @[JSON::Field(key: "$ref")]
     getter ref : String? = nil
 
     def initialize(@type : String? = nil, @format : String? = nil, @required : Array(String)? = nil,
                    @default : (String | Int32 | Int64 | Float64 | Bool)? = nil,
-                   @properties : Hash(String, Property)? = nil, @ref : String? = nil)
+                   @properties : Hash(String, Property)? = nil, @items : NamedTuple("$ref": String)? = nil,
+                   @ref : String? = nil)
     end
   end
 end
